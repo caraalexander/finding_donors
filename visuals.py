9c44135843e15bf67f467bbb99c400b068d094ea
Cara Alexander <cara@a4inc.ca>
2019-06-20 15:31:31 -0700

    Add feature importance extraction

--- visuals.py
+++ visuals.py
@@ -139,6 +139,7 @@ def feature_plot(importances, X_train, y_train):
     pl.bar(np.arange(5) - 0.3, np.cumsum(values), width = 0.2, align = "center", color = '#00A0A0', \
           label = "Cumulative Feature Weight")
     pl.xticks(np.arange(5), columns)
+    pl.xticks(rotation=45)
     pl.xlim((-0.5, 4.5))
     pl.ylabel("Weight", fontsize = 12)
     pl.xlabel("Feature", fontsize = 12)
